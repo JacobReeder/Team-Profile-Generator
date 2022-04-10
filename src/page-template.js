0dd18a007c76e90749bf8605e4a8c3f95cdea526
JacobReeder <jacob.reeder1@gmail.com>
2022-04-10 14:14:17 -0700

    added generated html/not functional

--- src/page-template.js
+++ src/page-template.js
@@ -1,68 +1,69 @@
-/////Include code to dynamically generate html to page
-////
+////include const of all 3 employee type and file paths
+const cardEngineer = require('../lib/engineer');
+const cardIntern = require('../lib/intern');
+const cardManager = require('../lib/manager');
 
-// create the about section
-const generateAbout = aboutText => {
-    if (!aboutText) {
-      return '';
-    }
+
   
-    return `
-      <section class="my-3" id="about">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-        <p>${aboutText}</p>
-      </section>
-    `;
-  };
+  // create the projects section //// refactor to a constructor function
+
+class htmlGen {
   
-  // create the projects section
-  const generateRoster = rosterArr => {
-    return `
-      <section class="my-3" id="portfolio">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-        <div class="flex-row justify-space-between">
-        ${rosterArr
+     newHtml(cardManager, cardEngineer, cardIntern) {
+
+     }
+}
+
+  
+    return ` ${cardManager
           .filter(({ feature }) => feature)
-          .map(({ name, id, email }) => {
+          .map(({ name, id, email, officeNumber }) => {
             return `
-            <div class="col-12 mb-2 bg-dark text-light p-3">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${email.map(email => email).join(',')}
-              </h5>
-              <p>${id}</p>
-            </div>
-          `;
-          })
-          .join('')}
-  
-        ${rosterArr
-          .filter(({ feature }) => !feature)
-          .map(({ name, id, email }) => {
-            console.log(languages);
-            return `
-            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${email.join(', ')}
-              </h5>
-              <p>${id}</p>
-            </div>
-          `;
-          })
-          .join('')}
-      
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+          </nav>
         </div>
-      </section>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateRoster(roster)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
     `;
-  };
+          }
+  })
+
   
+  ////////////////////////////////////////////////////////////////
+
   // export function to generate entire page
-  module.exports = templateData => {
-    // destructure page data by section
-    const { roster, about, ...header } = templateData;
+  /*module.exports = templateData => {
+ 
+
+     return ` ${cardEnginer
+          .filter(({ feature }) => feature)
+          .map(({ name, id, email,  }) => {
+            return `
   
     return `
     <!DOCTYPE html>
@@ -97,3 +98,47 @@ const generateAbout = aboutText => {
     </html>
     `;
   };
+
+///////////////////////////////////////////////////////////////////////
+
+     return ` ${cardIntern
+          .filter(({ feature }) => feature)
+          .map(({ name, id, email, school }) => {
+            return `
+  
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateRoster(roster)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+  };*/
+
+
+ 
